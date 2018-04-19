b4b55bc02858e0ede705b7372f5d6d319ce6f368
ne-il <neil.anteur@yahoo.fr>
2018-04-19 17:37:14 +0200

    APP_SETTINGS (prod, stage, dev) handling added

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+x = os.environ['APP_SETTINGS']
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    # return "Hello World!"
+    return str(os.environ['APP_SETTINGS'])
 
 @app.route('/<name>')
 def hello_name(name):
@@ -12,4 +17,6 @@ def hello_name(name):
 
 
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run()
+
