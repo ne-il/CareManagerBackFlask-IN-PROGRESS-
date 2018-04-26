7d39d4746eb8c651a03ae568c66151b3b5a90aad
ne-il <neil.anteur@yahoo.fr>
2018-04-26 17:09:12 +0200

    test oneToMany

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Patient
+from models import Patient, Document, Result
 
 
 
 @app.route('/')
 def hello():
     # return "Hello World!"
+    p = Patient()
+    d1 = Document()
+    d2 = Document()
+    p.documents.append(d1)
+    p.documents.append(d2)
+
+    s = str(p.documents)
+    return s
+
     return str(os.environ['APP_SETTINGS'])
 
 @app.route('/<name>')
--- models.py
+++ models.py
@@ -1,5 +1,24 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.orm import relationship
+
+import enum
+
+class DocumentType(enum.Enum):
+    prescription = "PRESCRIPTION"
+    radiograph = "RADIOGRAPH"
+    observation = "OBSERVATION"
+
+class DocumentStatus(enum.Enum):
+    in_progress = "IN_PROGRESS"
+    validated = "VALIDATED"
+
+class StaffType(enum.Enum):
+    doctor = "DOCTOR"
+    nurse = "NURSE"
+    secretary = "SECRETARY"
+    admin = "ADMIN"
+
 
 class Result(db.Model):
     __tablename__ = 'results'
@@ -23,6 +42,7 @@ class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String())
     lastName = db.Column(db.String())
+    documents = relationship("Document", back_populates="patient")
 
     def __init__(self, url, firstName, lastName):
         self.url = url
@@ -31,3 +51,50 @@ class Patient(db.Model):
 
     def __repr__(self):
         return '<id {}, firstName{}, lastName{} >'.format(self.id, self.firstName, self.lastName)
+
+
+
+class Document(db.Model):
+    __tablename__ = 'documents'
+
+    id = db.Column(db.Integer, primary_key=True)
+    # author = db.Column(Staff)
+    # patient = db.Column(Patient)
+
+    patient = relationship("Patient", back_populates="documents")
+    type = db.Column(db.Enum(DocumentType))
+    status = db.Column(db.Enum(DocumentStatus))
+    description = db.Column(db.String())
+    url_image = db.Column(db.String())
+    validation_ts = db.Column(db.TIMESTAMP);
+
+
+    def __init__(self, id, patient, type, status, description, url_image):
+        # self.author= author
+        self.patient = patient
+        self.type = type
+        self.status = status
+        self.description = description
+
+    def __repr__(self):
+        return '<id: {}, author{}, patient{}, description{}, status{} >'.format(self.id, self.author, self.patient, self.description, self.status)
+
+
+# class Staff(db.Model):
+#     __tablename__ = 'staffs'
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     firstName = db.Column(db.String())
+#     lastName = db.Column(db.String())
+#     type = db.Column(db.Enum(StaffType))
+#
+#     def __init__(self, firstName, lastName, type):
+#         self.firstName = firstName
+#         self.lastName = lastName
+#         self.type = type
+#
+#     def __repr__(self):
+#         return '<id {}, firstName{}, lastName{}, type{} >'.format(self.id, self.firstName, self.lastName, self.type)
+
+
+
